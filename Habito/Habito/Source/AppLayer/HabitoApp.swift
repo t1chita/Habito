fc5af2dc2a36954ed50c8ef875bcadea1d5a45ac
Temur Chitashvili <temur.chitashvilii@gmail.com>
2024-11-05 03:11:24 +0400

    inital commit

---
+++ Habito/Habito/Source/AppLayer/HabitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitoApp.swift
+//  Habito
+//
+//  Created by Temur Chitashvili on 05.11.24.
+//
+
+import SwiftUI
+
+@main
+struct HabitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
